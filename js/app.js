658bdb122f024a5b02f6d53c35e672c9992a42b9
Ponis <johndoe@example.com>
2021-10-04 13:38:50 -0300

    wp 1-7

---
+++ js/app.js
@@ -0,0 +1,42 @@
+//1
+alert("Hola Mundo");
+
+//2
+document.write("Hola Mundo <br>");
+
+//3
+document.write(3+5 +"<br>");
+
+/*
+//4
+var nombre = prompt("¿Cual es tu nombre?");
+document.write("Hola "+ nombre);
+*/
+
+//5
+var num1 = parseInt(prompt("Ingrese un primer numero"));
+// var num2 = parseInt(prompt("Ingrese un segundo numero"));
+// var num3 = parseInt(prompt("Ingrese un tercer numero"));
+// document.write(num1+num2);
+
+/*
+//6
+if (num1>num2) {
+ alert("El numero mayor es: "+num1)   
+} else {
+alert("El numero mayor es: "+num2)
+}
+*/
+
+/*
+//7
+if (num1>num2 && num1>num3) {
+    alert("El numero mayor es: "+num1)   
+} else if(num2>num1 && num2>num3){
+    alert("El numero mayor es: "+num2) 
+}  else {
+  alert("El numero mayor es: "+num3) 
+}
+*/
+
+//8
