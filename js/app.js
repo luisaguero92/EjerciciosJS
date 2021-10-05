5577c1623456c30044735d30a1675ecd48c3586f
Ponis <johndoe@example.com>
2021-10-05 13:42:35 -0300

    wp

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ alert("Hola Mundo");
 document.write("Hola Mundo <br>");
 
 //3
-document.write(3+5 +"<br>");
+document.write(3 + 5 + "<br>");
 
 /*
 //4
@@ -41,10 +41,42 @@ if (num1>num2 && num1>num3) {
 
 //8 Numero divisible entre 2
 
-if (num1%2 == 0) {
-  alert("El numero es divisible entre 2 o par")
-}  else{
+/*
+var
+if (num1 % 2 == 0) {
+
+} else {
   alert("El numero no divisible entre 2 o inpar")
 }
+*/
 
-//9 
+//13
+var verificacion2357 = []
+if (num1 % 2 == 0) {
+  verificacion2357.push(1)
+} else {
+  verificacion2357.push(0)
+} 
+if (num1 % 3 == 0) {
+  verificacion2357.push(1)
+} else {
+  verificacion2357.push(0)
+}
+ if (num1 % 5 == 0) {
+  verificacion2357.push(1)
+} else {
+  verificacion2357.push(0)
+} 
+if(num1 % 7 == 0) {
+  verificacion2357.push(1)
+} else {
+  verificacion2357.push(0)
+}
+
+alert("Numero verificado: " + verificacion2357);
+document.write(verificacion2357);
+if (condition) {
+  
+} else {
+  
+}
